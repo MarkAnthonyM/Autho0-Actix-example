710a86c5cb398499c02852801bbb3e1e83ce545c
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-11-21 18:14:35 -0600

    Update http address

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ async fn main() -> std::io::Result<()> {
             .route("/users", web::post().to(handlers::add_user))
             .route("/users/{id}", web::delete().to(handlers::delete_user))
     })
-    .bind("127.0.0.1:8000")?
+    .bind("127.0.0.1:8080")?
     .run()
     .await
 }
